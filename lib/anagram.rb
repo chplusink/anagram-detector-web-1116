e0bbb4465376181acfb0f2749cca7dac7185e78f
VIC LAM <viclam@VICs-MacBook-Pro.local>
2016-11-30 18:17:16 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+	attr_reader :word
+
+	def initialize(word)
+		@word = word
+	end
+
+	def match(words)
+		words.select do |w|
+			sort_word(word) == sort_word(w)
+		end
+	end
+
+	def sort_word(word)
+		word.split("").sort.join("")
+	end
+
+end
